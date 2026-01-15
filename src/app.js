b58cc489eb8824e8063ee6ae59a5838592f671fd
Thalia Lourena de Lima <159783544+AriFagundes@users.noreply.github.com>
2026-01-15 05:35:38 -0300

    Adicionar localhost:3002 CORS

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ const corsOptions = {
             'http://clientes.greatic.io',
             'http://localhost:3000',
             'http://localhost:3001',
+            'http://localhost:3002',
             'http://localhost:5173',
             'http://127.0.0.1:5173',
             'http://127.0.0.1:5500',
